c483d14723862794a3a91c6b3fad73dcbd4e1499
temitopeohassan <temitope.hassan.2@gmail.com>
2024-11-22 15:50:19 +0100

    dark mode logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,20 +6,25 @@ import Image from "next/image";
 import Link from "next/link";
 import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import { useTheme } from "next-themes";
 
 export default function Navbar() {
+  const { theme } = useTheme();
+
   return (
     <nav className="border-b">
       <div className="flex h-16 items-center px-4 container mx-auto">
         <Link href="/" className="font-semibold text-xl mr-6">
-          <Image alt="Melobanc Labs" className="cursor-pointer" height={50} width={438} src="/logo.png" />
+          <Image 
+            alt="Melobanc Labs" 
+            className="cursor-pointer" 
+            height={50} 
+            width={438} 
+            src={theme === 'dark' ? "/logo-black.png" : "/logo.png"}
+          />
         </Link>
         
         <div className="flex items-center space-x-4 flex-1">
-          <div className="relative w-full max-w-sm">
-            <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input placeholder="Search creators..." className="pl-8" />
-          </div>
           <nav className="flex items-center space-x-4 mx-6">
             <Link href="/explore" className="text-sm font-medium">
               Explore
